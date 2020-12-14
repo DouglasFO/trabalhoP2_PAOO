42a383256dc2fba862c2d7cb18e0f3a6408567a0
DouglasFO <douglasfranca2290@gmail.com>
2020-12-14 01:32:54 -0300

    Atualizando o trabalho com as novas funções aprendidas nas aulas e arquivos fontes

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Página Inicial</h1>
+</template>
+<script>
+export default {
+    name: "hqsCrud",
+    data: () => ({}),
+    methods: {},
+};
+</script>
--- src/components/hqsCrud.vue
+++ src/components/hqsCrud.vue
@@ -2,15 +2,75 @@
     <v-container>
         <v-row class="text-center">
             <v-col cols="12">
-                <h1>Loja de Quadrinhos</h1>
-                <v-btn depressed color="primary">
-                    Inserir
-                </v-btn>            
+                <h1>Loja de Quadrinhos</h1>            
             </v-col>
         </v-row>
         <v-row class="text-center">
             <v-col cols="12">
                 <v-data-table dense :headers="headers" :items="users" item-key="name" class="elevation-1">
+                      <template v-slot:top>
+                          <v-toolbar flat>
+                            <v-toolbar-title>Lista de quadrinhos</v-toolbar-title>
+                            <v-divider class="mx-4" inset vertical></v-divider>
+                            <v-spacer></v-spacer>
+                            <v-dialog v-model="dialog" max-width="700px">
+                              <template v-slot:activator="{ on, attrs }">
+                                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                                  Novo Item
+                                </v-btn>
+                              </template>
+                              <v-card>
+                                <v-card-title>
+                                  <span class="headline">{{ formTitle }}</span>
+                                </v-card-title>
+
+                                <v-card-text>
+                                  <v-container>
+                                    <v-row>
+                                      <v-col cols="12" sm="6" md="4">
+                                        <v-text-field
+                                          v-model="editedItem.id"
+                                          label="Id"
+                                        ></v-text-field>
+                                      </v-col>
+                                      <v-col cols="12" sm="6" md="8">
+                                        <v-text-field
+                                          v-model="editedItem.name"
+                                          label="Nome"
+                                        ></v-text-field>
+                                      </v-col>
+                                      <v-col cols="12" sm="6" md="4">
+                                        <v-text-field
+                                          v-model="editedItem.qtde"
+                                          label="Quantidade"
+                                        ></v-text-field>
+                                      </v-col>
+                                      <v-col cols="12" sm="6" md="4">
+                                        <v-text-field
+                                          v-model="editedItem.price"
+                                          label="Preço"
+                                        ></v-text-field>
+                                      </v-col>
+                                    </v-row>
+                                  </v-container>
+                                </v-card-text>
+
+                                <v-card-actions>
+                                  <v-spacer></v-spacer>
+                                  <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+                                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+                                </v-card-actions>
+                              </v-card>
+                            </v-dialog>
+                          </v-toolbar>
+                    </template>
+                    <template v-slot:item.actions="{ item }">
+                      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+                      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                      <v-btn color="primary" @click="initialize"> Reset </v-btn>
+                    </template>
                 </v-data-table>
             </v-col>
         </v-row>
@@ -20,18 +80,30 @@
 <script>
 import axios from "axios";
 export default {
-    name: "UserCrud",
+    name: "hqsCrud",
     data: () => ({
+            dialog: false,
             headers: [
                 {text: "Nome", value: "name"},
                 {text: "Preço", value: "price"},
                 {text: "Quantidade", value: "qtde"},
-                {text: "Actions", value: "icon"}
+                { text: "Actions", value: "actions", sortable: false },
             ],
             users: [
-                {id: 1, name: "Douglas", location: "Rio de Janeiro"},
-                {id: 1, name: "João", location: "Bahia"}
-            ],
+                {id: 1, name: "Dragon Ball - Edição Definitiva 1", qtde: "12", price: "R$ 64,90"}],
+                editedIndex: -1,
+                editedItem: {
+                  id: 0,
+                  name: "HQ/Mangá",
+                  qtde: "0",
+                  price: "R$ 29,90",
+                },
+                defaultItem: {
+                  id: 0,
+                  name: "HQ/Mangá",
+                  qtde: "0",
+                  price: "R$ 00,00",
+                },        
     }),
     methods: {
         inicializa() {
@@ -40,7 +112,71 @@ export default {
                     this.users = response.data;
             })
             .catch((error)=> console.log(error));
-        }
+        },
+        close() {
+          this.dialog = false;
+          setTimeout(() => {
+            this.editedItem = Object.assign({}, this.defaultItem);
+            this.editedIndex = -1;
+          }, 300);
+        },
+        save() {
+              if (this.editedIndex > -1) {
+                //alteracao
+                //console.log("Item ID Teste: "+this.editedItem.id);
+                axios
+                  .put(
+                    "http://localhost:3000/users/" + this.editedItem.id,
+                    this.editedItem
+                  )
+                  .then((response) => {
+                    console.log(response);
+                    Object.assign(this.users[this.editedIndex], this.editedItem);
+                    this.close();
+                  })
+                  .catch((error) => console.log(error));
+              } else {
+                //Inclusao
+                axios
+                  .post("http://localhost:3000/users", this.editedItem)
+                  .then((response) => {
+                    console.log(response);
+                    this.users.push(this.editedItem);
+                    this.close();
+                  })
+                  .catch((error) => console.log(error));
+              }
+         },
+         editItem(item) {
+              this.editedIndex = this.users.indexOf(item);
+              this.editedItem = Object.assign({}, item);
+              this.dialog = true;
+          },
+          deleteItem(item) {
+              const index = this.users.indexOf(item);
+              confirm("Deseja apagar este item?") &&
+                axios
+                  .delete("http://localhost:3000/users/" + item.id)
+                  .then((response) => {
+                    console.log(response.data);
+                    this.users.splice(index, 1);
+                  })
+                  .catch((error) => console.log(error));
+          },
+    },
+    computed: {
+        formTitle() {
+          return this.editedIndex === -1 ? "Novo Item" : "Editar Item";
+        },
+    },
+
+    watch: {
+        dialog(val) {
+          val || this.close();
+        },
+        dialogDelete(val) {
+          val || this.closeDelete();
+        },
     },
     created() {
         this.inicializa();
